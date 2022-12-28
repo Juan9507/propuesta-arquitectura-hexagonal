35e525c143fb7624fab690f7a2f3aa88eec839e5
juan9507 <juandavidnaranjo75@gmail.com>
2022-12-28 10:07:02 -0500

    Se genero un modelo (usuario)

---
+++ domain/model/src/main/java/co/com/example/model/usuario/Usuario.java
@@ -0,0 +1,11 @@
+package co.com.example.model.usuario;
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * Entidad del dominio
+ */
+@Data
+@Builder(toBuilder = true)
+public class Usuario {
+}
---
+++ domain/model/src/main/java/co/com/example/model/usuario/gateways/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package co.com.example.model.usuario.gateways;
+
+/**
+ * Interface repository para usar los metodos de la base de datos
+ * que sera implementada en infraestructura por el repositoryAdapter
+ */
+public interface UsuarioRepository {
+}
