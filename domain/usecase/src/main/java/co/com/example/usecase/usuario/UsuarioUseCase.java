28e204dc86bf9e81c1211d4e77afc885a1d71c15
juan9507 <juandavidnaranjo75@gmail.com>
2022-12-28 10:11:49 -0500

    Se creo un caso de uso vacio

---
+++ domain/usecase/src/main/java/co/com/example/usecase/usuario/UsuarioUseCase.java
@@ -0,0 +1,12 @@
+package co.com.example.usecase.usuario;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Caso de uso para implementar la logica de negocio
+ * haciendo uso del UsuarioRepository
+ */
+
+@RequiredArgsConstructor
+public class UsuarioUseCase {
+}
